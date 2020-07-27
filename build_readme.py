0555e9e8f0c8025b9afbe6f12a62cad924aa836b
siguser <chaoshi@synopsys.com>
2020-07-27 16:43:56 +0800

    get project from hub and print to readme.md

--- build_readme.py
+++ build_readme.py
@@ -6,12 +6,31 @@ import pathlib
 import re
 import os
 import datetime
+from blackduck.HubRestApi import HubInstance
+import json
+import pprint
+
 
 root = pathlib.Path(__file__).parent.resolve()
 
 
 if __name__ == "__main__":
-    readme = root / "README.md"
 
-    rewritten = "demo hub status === "
+
+    urlbase = "https://poc94.blackduck.synopsys.com/"
+    TOKEN = os.environ.get("HUB_94_TOKEN", "")
+    hub = HubInstance(urlbase, api_token=TOKEN, insecure=True)
+
+    projects = hub.get_projects()
+
+    print(json.dumps(projects.get('items', [])))
+
+
+
+    readme = root / "README.md"
+    pp = pprint.PrettyPrinter(indent=4)
+
+    rewritten = "demo hub status === \n"
+    rewritten += pp.pprint(json.dumps(projects.get('items', [])))
+
     readme.open("w").write(rewritten)
